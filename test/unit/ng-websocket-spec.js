9d9a3901aa160984044047c07db621b2927e8b63
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-08-26 23:51:05 +0200

    added async first test

--- test/unit/ng-websocket-spec.js
+++ test/unit/ng-websocket-spec.js
@@ -1,14 +1,48 @@
 'use strict';
 
+var $websocket;
+
 describe('Testing ng-websocket', function () {
     beforeEach(module('ngWebsocket'));
+    beforeEach(inject(function (_$websocket_) {
+        $websocket = _$websocket_;
+    }));
 
-    describe('Testing $websocket service', function () {
-        it('should create a connecting WebSocket', inject(function ($websocket) {
-            var ws = $websocket.$new('ws://localhost:12345');
+    describe('Testing a non-working websocket', function () {
+        var ws;
 
+        beforeEach(function () {
+            ws = $websocket.$new('ws://localhost:12345');
+        });
+
+        afterEach(function () {
+            ws.$close();
+        });
+
+        it('should create a connecting WebSocket', function () {
             expect(ws).toBeDefined();
             expect(ws.$status()).toEqual(ws.$CONNECTING);
-        }));
+        });
+    });
+
+    describe('Testing $websocket service', function () {
+        var ws;
+
+        beforeEach(function (done) {
+            ws = $websocket.$new('ws://localhost:12345');
+
+            ws.$on('$open', function () {
+                done();
+            });
+        });
+
+        afterEach(function () {
+            ws.$close();
+        });
+
+        it('should have an open connection', function (done) {
+            expect(ws.$status()).toEqual(ws.$OPEN);
+            done();
+        });
     });
 });
