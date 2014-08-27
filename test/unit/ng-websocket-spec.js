e5b72d1690d6e84c277ee9ae9e9b8cd06d087880
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-08-27 23:09:03 +0200

    added parrot websocket server test

--- test/unit/ng-websocket-spec.js
+++ test/unit/ng-websocket-spec.js
@@ -71,4 +71,40 @@ describe('Testing ng-websocket', function () {
             expect(error).toBeNull();
         });
     });
+
+    describe('Testing a parrot websocket server', function () {
+        var ws;
+
+        beforeEach(function (done) {
+            ws = $websocket.$new('ws://localhost:12345');
+
+            ws.$on('$open', function () {
+                done();
+            });
+        });
+
+        afterEach(function () {
+            ws.$close();
+        });
+
+        it('should have received the same message', function (done) {
+            ws.$emit('custom event', 'hello world');
+
+            ws.$on('$message', function (message) {
+                expect(message.data).toEqual('hello world');
+
+                done();
+            });
+        });
+
+        it('should have received the same message on the same event', function (done) {
+            ws.$emit('custom event', 'hello world');
+
+            ws.$on('custom event', function (message) {
+                expect(message).toEqual('hello world');
+
+                done();
+            });
+        });
+    });
 });
