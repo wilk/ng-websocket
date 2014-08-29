9894ee5cee7ea07158f15b6f7f9e6fc989fee9d4
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-08-29 12:31:26 +0200

    added lazy configuration

--- ng-websocket.js
+++ ng-websocket.js
@@ -8,11 +8,30 @@ angular
         function $websocketManager () {
             var wsm = this;
 
-            wsm.websocketList = {};
+            wsm.$$websocketList = {};
+            wsm.$$config = {
+                lazy: false
+            };
+
+            wsm.$setup = function (cfg) {
+                cfg = cfg || {};
+                wsm.$$config.lazy = cfg.lazy || wsm.$$config.lazy;
+            };
 
             wsm.$new = function (cfg) {
-                var ws = new $websocket(cfg);
-                wsm.websocketList[cfg] = ws;
+                var wsCfg = angular.copy(wsm.$$config);
+
+                if (typeof cfg === 'string') {
+                    wsCfg.url = cfg;
+                }
+                else if (typeof cfg === 'object') {
+                    for (var prop in cfg) {
+                        if (wsCfg.hasOwnProperty(prop)) wsCfg[prop] = cfg[prop];
+                    }
+                }
+
+                var ws = new $websocket(wsCfg);
+                wsm.$$websocketList[wsCfg.url] = ws;
 
                 return ws;
             };
@@ -22,6 +41,8 @@ angular
             var me = this;
 
             me.$$eventMap = {};
+            me.$$config = cfg;
+            me.$$ws = undefined;
 
             me.$$fireEvent = function () {
                 var args = [];
@@ -37,32 +58,30 @@ angular
             me.$$init = function (cfg) {
                 if (typeof cfg === 'undefined') throw new Error('An url must be specified for WebSocket');
 
-                if (typeof cfg === 'string') {
-                    me.ws = new WebSocket(cfg);
+                me.$$ws = new WebSocket(cfg.url);
 
-                    me.ws.onmessage = function (message) {
-                        try {
-                            var decoded = JSON.parse(message.data);
-                            me.$$fireEvent(decoded.event, decoded.data);
-                            me.$$fireEvent('$message', decoded);
-                        }
-                        catch (err) {
-                            me.$$fireEvent('$message', message.data);
-                        }
-                    };
+                me.$$ws.onmessage = function (message) {
+                    try {
+                        var decoded = JSON.parse(message.data);
+                        me.$$fireEvent(decoded.event, decoded.data);
+                        me.$$fireEvent('$message', decoded);
+                    }
+                    catch (err) {
+                        me.$$fireEvent('$message', message.data);
+                    }
+                };
 
-                    me.ws.onerror = function (error) {
-                        me.$$fireEvent('$error', error);
-                    };
+                me.$$ws.onerror = function (error) {
+                    me.$$fireEvent('$error', error);
+                };
 
-                    me.ws.onopen = function () {
-                        me.$$fireEvent('$open');
-                    };
+                me.$$ws.onopen = function () {
+                    me.$$fireEvent('$open');
+                };
 
-                    me.ws.onclose = function () {
-                        me.$$fireEvent('$close');
-                    };
-                }
+                me.$$ws.onclose = function () {
+                    me.$$fireEvent('$close');
+                };
 
                 return me;
             };
@@ -71,6 +90,11 @@ angular
             me.$OPEN = 1;
             me.$CLOSING = 2;
             me.$CLOSED = 3;
+
+            me.$open = function () {
+                if (me.$status() !== me.$OPEN) me.$$init(me.$$config);
+                return me;
+            };
 
             me.$on = function (event, handler) {
                 if (typeof event !== 'string' || typeof handler !== 'function') throw new Error('$on accept two parameters: a String and a Function');
@@ -96,26 +120,29 @@ angular
                     data: data
                 };
 
-                me.ws.send(JSON.stringify(message));
+                me.$$ws.send(JSON.stringify(message));
 
                 return me;
             };
 
             me.$close = function () {
-                me.ws.close();
+                if (me.$status() !== me.$CLOSED) me.$$ws.close();
 
                 return me;
             };
 
             me.$status = function () {
-                return me.ws.readyState;
+                if (typeof me.$$ws === 'undefined') return me.$CLOSED;
+                else return me.$$ws.readyState;
             };
 
             me.$ready = function () {
                 return me.$status() === me.$OPEN;
             };
 
-            return me.$$init(cfg);
+            if (!me.$$config.lazy) me.$$init(me.$$config);
+
+            return me;
         }
 
         wsp.$get = function () {
