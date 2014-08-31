e31f77ef331be2e317352e90c20db9c1e7426711
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-08-31 22:51:57 +0200

    wrap the server into a node module

--- demo/server.js
+++ demo/server.js
@@ -1,11 +1,29 @@
 'use strict';
 
-var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: 12345});
+var WebSocketServer = require('ws').Server;
 
-wss.on('connection', function (ws) {
-    ws.on('message', function (msg) {
-        console.dir(msg);
-        ws.send(msg);
-    });
-});
+function Server () {
+    var me = this;
+
+    me.start = function () {
+        var wss = new WebSocketServer({port: 12345});
+
+        wss.on('connection', function (ws) {
+            ws.on('message', ws.send);
+        });
+
+        return wss;
+    };
+
+    me.stop = function () {
+        process.exit(0);
+    };
+
+    return me;
+}
+
+var server = module.exports = new Server();
+
+if (require.main === module) {
+    server.start();
+}
