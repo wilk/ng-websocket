d7e5e9865770af1e2e661407a538317c064df9f7
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-08-31 22:43:39 +0200

    added gulp support

---
+++ Gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    karma = require('gulp-karma'),
+    server = require('./demo/server');
+
+gulp.task('test', function () {
+    server.start();
+    
+    return gulp.src([
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'ng-websocket.js',
+            'test/unit/**/*.js'
+        ])
+        .pipe(karma({
+            configFile: 'test/karma.conf.js',
+            action: 'run'
+        }))
+        .on('error', server.stop)
+        .on('end', server.stop);
+});
