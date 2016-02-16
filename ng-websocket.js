4bffe1433a1e7dc3e2d956143f5eded2dab45c2e
kaufholdr <richard@kaufhold.me>
2016-02-16 16:49:18 +0100

    Added removal of closed sockets from cache.

--- ng-websocket.js
+++ ng-websocket.js
@@ -250,6 +250,8 @@
         };
 
         me.$close = function () {
+            removeFromCache();
+
             if (me.$status() !== me.$CLOSED) me.$$ws.close();
 
             if (me.$$reconnectTask) {
